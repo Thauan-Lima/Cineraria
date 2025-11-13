74cfc4ad8e035b4cb7591740ae30162311759b43
Thauan-Lima <oliveira.thauan@escolar.ifrn.edu.br>
2025-11-13 11:42:55 -0300

    Refactor SelectOne class for product retrieval

--- JDBC/JAVA/SelectOne.java
+++ JDBC/JAVA/SelectOne.java
@@ -5,46 +5,40 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-public class ExemploSelectOne {
+public class SelectOne {
 
 	public static void main(String[] args) {
 
 		String url = "jdbc:mysql://localhost/escola";
-		String usuario = "root";
-		String senha = "123456";
+		String usuario = "20231164010027";
+		String senha = "20231164010027+lima";
 
-		String sql = "select * from aluno where matricula = ?";
+		String sql = "select * from produto where id = ?";
 
 		try (Connection conexao = DriverManager.getConnection(url, usuario, senha);
 				PreparedStatement stmt = conexao.prepareStatement(sql);
 				Scanner leitor = new Scanner(System.in)) {
 
-			System.out.print("Digite a matrícula da busca: ");
-			var matricula_entrada = leitor.nextInt();
+			System.out.print("Digite o id da busca: ");
+			var id_entrada = leitor.nextInt();
 
-			stmt.setInt(1, matricula_entrada);
+			stmt.setInt(1, id_entrada);
 
 			try (ResultSet rs = stmt.executeQuery()) {
 				if (rs.next()) {
 					var id = rs.getInt("id");
 					var nome = rs.getString("nome");
-					var matricula = rs.getInt("matricula");
-					var ira = rs.getDouble("ira");
-					var curso = rs.getString("curso");
-					var sexo = rs.getString("sexo");
-					var nascimento = rs.getString("nascimento");
-					var cadastro = rs.getString("cadastro");
+					var valor = rs.getDouble("valor");
+					var quantidade = rs.getInt("quantidade");
+					var medida = rs.getString("medida");
 
 					System.out.println("ID: " + id);
 					System.out.println("Nome: " + nome);
-					System.out.println("Matrícula: " + matricula);
-					System.out.println("IRA: " + ira);
-					System.out.println("Curso: " + curso);
-					System.out.println("Sexo: " + sexo);
-					System.out.println("Data de Nascimento: " + nascimento);
-					System.out.println("Data de Cadastro: " + cadastro);
+					System.out.println("Valor: " + valor);
+					System.out.println("Quantidade: " + quantidade);
+					System.out.println("Medida: " + medida);
 				} else {
-					System.out.println("Matrícula não encontrada!");
+					System.out.println("ID não encontrado!");
 				}
 			}
 
