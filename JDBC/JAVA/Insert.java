3d84eea4fffad69ff0850bafddb53b7a7cb7eb96
Thauan-Lima <oliveira.thauan@escolar.ifrn.edu.br>
2025-11-13 11:42:12 -0300

    Refactor Insert class for product insertion

--- JDBC/JAVA/Insert.java
+++ JDBC/JAVA/Insert.java
@@ -4,40 +4,37 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-public class ExemploInsert {
+public class Insert {
 
 	public static void main(String[] args) {
 
 		String url = "jdbc:mysql://localhost/escola";
-		String usuario = "root";
-		String senha = "123456";
+		String usuario = "20231164010027";
+		String senha = "20231164010027+lima";
 
-		String sql = "insert into aluno (nome, matricula, ira, sexo, nascimento) values (?, ?, ?, ?, ?)";
+
+		String sql = "insert into produto (nome, valor, quantidade, medida) values (?, ?, ?, ?)";
 
 		try (Connection conexao = DriverManager.getConnection(url, usuario, senha);
 				PreparedStatement stmt = conexao.prepareStatement(sql);
 				Scanner leitor = new Scanner(System.in)) {
 
-			System.out.print("Digite o nome: ");
+			System.out.print("Digite o nome do produto: ");
 			var nome = leitor.nextLine();
 
-			System.out.print("Digite a matrícula: ");
-			var matricula = leitor.nextInt();
+			System.out.print("Digite o valor do produto: ");
+			var valor = leitor.nextDouble();
 
-			System.out.print("Digite o IRA: ");
-			var ira = leitor.nextDouble();
+			System.out.print("Digite a quantidade do produto: ");
+			var quantidade = leitor.nextInt();
 
-			System.out.print("Digite o sexo: ");
-			var sexo = leitor.next();
-
-			System.out.print("Digite a data de nascimento (aaaa-mm-dd): ");
-			var nascimento = leitor.next();
+			System.out.print("Digite a medida do produto: ");
+			var medida = leitor.nextLine();
 
 			stmt.setString(1, nome);
-			stmt.setInt(2, matricula);
-			stmt.setDouble(3, ira);
-			stmt.setString(4, sexo);
-			stmt.setString(5, nascimento);
+			stmt.setDouble(2, valor);
+			stmt.setInt(3, quantidade);
+			stmt.setString(4, medida);
 
 			var linhas = stmt.executeUpdate();
 			System.out.println("Linhas afetadas = " + linhas);
